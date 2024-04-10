9fb7e30047eac5da69d3bf39ddde0499e18d0700
Seillemar <braguettefield@gmail.com>
2024-04-10 10:56:05 +0200

    page accueil

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ app.set('view engine', 'ejs'); // Use EJS as the template engine
 app.use('/styles', express.static(path.join(__dirname, 'templates', 'styles')));
 app.use('/scripts', express.static(path.join(__dirname, 'templates', 'scripts')));
 app.use('/images', express.static(path.join(__dirname, 'templates', 'images')));
+app.use('/video', express.static(path.join(__dirname, 'templates', 'video')));
 
 // Middleware to include header and footer on all pages
 app.use((req, res, next) => {
     res.locals.header = 'partials/header'; // Define the header partial
     res.locals.footer = 'partials/footer'; // Define the footer partial
+    res.locals.head = 'partials/head'; // Define the footer partial
     next();
 });
 
@@ -25,36 +27,15 @@ app.get('/home', (req, res) => {
     res.render('home');
 });
 
-// Route for the brackets page
-app.get('/planning-bracket', async (req, res) => {
-    try {
-        // Code to retrieve tournament data from Challonge API
-        const response = await axios.get('https://api.challonge.com/v1/tournaments/snib5or9.json', {
-            auth: {
-                username: 'Seillemar',
-                password: '5G41TwoPtpUfCoQmrtf0NumYwoKGXO5fHYQf1TfK', // Needed 'x' as password for API key authentication
-            },
-        });
-        const tournamentData = response.data;
 
-        console.log('Tournament data:', tournamentData)
-
-        // Code to render the bracket page and pass tournament data to the view
-        res.render('planning-bracket', { tournamentData });
-    } catch (error) {
-        console.error('Error fetching tournament data:', error);
-        res.status(500).send('Error fetching tournament data');
-    }
+// Route for the staff page
+app.get('/staff', (req, res) => {
+    res.render('staff');
 });
 
-// Route for the VODs page
-app.get('/vods', (req, res) => {
-    res.render('vods');
-});
-
-// Route for the partners page
-app.get('/partenaires', (req, res) => {
-    res.render('partenaires');
+// Route for the contact page
+app.get('/contact', (req, res) => {
+    res.render('contact');
 });
 
 // Start the server
